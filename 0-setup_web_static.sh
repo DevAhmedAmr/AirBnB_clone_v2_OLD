e5d84a9a2a006cbf85de2e1838710904a4afa978
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-03-15 08:04:55 +0200

    (automated command)

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,3 @@
 #!/usr/bin/env bash
 #script to install and configue nginx
-hostname=$(hostname)
 sudo apt update -y
-sudo apt-get -y install nginx
-
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-
-if [ -L "/data/web_static/current" ] && [ -e "/data/web_static/current" ]; then
-    sudo rm -r "/data/web_static/current"
-fi
-
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-
-# Check if the user exists
-username="ubuntu"
-
-if id "$username" &>/dev/null; then
-    echo "User $username already exists."
-else
-    # Create the user
-    sudo useradd -m "$username"
-    echo "User $username created."
-fi
-
-sudo systemctl restart nginx
