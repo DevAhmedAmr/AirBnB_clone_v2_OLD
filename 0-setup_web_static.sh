3d2ab9ee2c9429478c276415b73b289a7d67c39c
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-03-15 06:19:31 +0200

    (automated command)

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,75 +1,59 @@
 #!/usr/bin/env bash
-# Script to install and configure nginx
-
+#script to install and configue nginx
 hostname=$(hostname)
-
-# Update package lists and install nginx
-sudo apt update
+sudo apt update -y
 sudo apt-get -y install nginx
-
-# Allow Nginx HTTP traffic through the firewall
+sudo ufw app list
 sudo ufw allow 'Nginx HTTP'
 
-# Create necessary directories
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
-# Create index.html and custom_404.html
 echo "Hello World!" | sudo tee /var/www/html/index.html
 echo "Ceci n'est pas une page" | sudo tee /usr/share/nginx/html/custom_404.html
 
-# Create test index.html
 sudo printf %s "<html>
 <head>
 </head>
-<body> Hello, this is a test page </body>
+<body> Hello,this is a test page </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Remove existing symlink if exists
 if [ -L "/data/web_static/current" ] && [ -e "/data/web_static/current" ]; then
-	sudo rm -r "/data/web_static/current"
+	rm -r "/data/web_static/current"
 fi
 
-# Create symlink to current release
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
-# Change ownership of /data directory
 sudo chown -R ubuntu:ubuntu /data/
 
-# Configure Nginx
-sudo tee /etc/nginx/sites-available/default >/dev/null <<EOF
-server {
-    listen 80;
-    listen [::]:80;
+printf %s "server {
+        listen 80;
+        listen [::]:80;
 
-    server_name $hostname;
+        server_name $hostname;
 
-    root /var/www/html;
+        root /var/www/html;
 
-    index index.html;
+        index index.html;
 
-    error_page 404 /custom_404.html;
+        error_page 404 /custom_404.html;
+        
 
-    add_header X-Served-By $hostname;
+        add_header X-Served-By $hostname;
 
-    location = /custom_404.html {
+        location = /custom_404.html {
         root /usr/share/nginx/html;
-        internal;
-    }
+        internal;}
 
-    location /redirect_me {
-        return 301 http://google.com/;
-    }
+        location /redirect_me {
+            return 301 http://google.com/;
+                                            }
+        location /hbnb_static{
+            alias /data/web_static/current/;}
 
-    location /hbnb_static {
-        alias /data/web_static/current/;
-    }
+        location / {
+            try_files \$uri \$uri/ =404;
+            }
+        }" >/etc/nginx/sites-available/default
 
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-}
-EOF
-
-# Restart Nginx
 sudo systemctl restart nginx
