7ad80b4bc87d83d331eb87f461272e0e3c9225a9
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-03-16 10:36:07 +0200

    Refactor do_pack function in 1-pack_web_static.py
    Refactor the do_pack function in 1-pack_web_static.py to improve readability
    and maintainability by extracting code into separate functions for creating
    folders and generating the tgz file name.

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,20 +2,25 @@
 """
 doc
 """
-
+from os.path import exists
 from datetime import datetime
+import os
 from fabric.api import local
-from os.path import isdir
+
+
+def create_folder(name):
+    if not exists(f"./{name}"):
+        os.mkdir(name)
+
+
+def create_tgz_name():
+    time = datetime.now()
+    current_time = time.strftime("%Y%m%d%H%M%S")
+    return "web_static_" + current_time + ".tgz"
 
 
 def do_pack():
-    """doc"""
-    try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except:
-        return None
+    create_folder("versions")
+    tgz_fileName = create_tgz_name()
+    output = local(f"tar -czvf {tgz_fileName}.tar.gz ./web_static", capture=True)
+    return f"./{tgz_fileName}.tar.gz"
